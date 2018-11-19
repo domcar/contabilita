451b08978e982b5f8ce45d278491399591126595
Domenico Caruso <domenico.caruso@de.clara.net>
2018-11-19 19:30:59 +0100

    test

--- prova.py
+++ prova.py
@@ -1,23 +1,31 @@
+import json
+
+dizionario = {}
 
 spese = 0
 with open("estrattoConto.csv") as f: 
    for line in f.readlines():
-     #print("Line {}: {}".format(cnt, line.strip()))
      data1 = line.split(";")[0]
      try:
        mese = data1.split(".")[1]
        anno = data1.split(".")[2]
-     except:
-       continue
+     except Exception as e:
+       print ("warning", e)
+
      data2 = line.split(";")[1]
-     richiedente = line.split(";")[2]
+     richiedente = line.split(";")[2].upper()
      testo = line.split(";")[3]
      causale = line.split(";")[4]
      valuta = line.split(";")[6]
-     if "REWE" in richiedente.upper():
+     try:
        importo = line.split(";")[5]
        importo = float(importo.replace(",","."))
-       spese = spese + importo
-       print importo
-print spese
+     except:
+       continue
+     if not richiedente in dizionario.keys():
+       dizionario[richiedente] = importo
+     else:
+       dizionario[richiedente] += importo
+
+print json.dumps(dizionario,sort_keys=True, indent=4)
 
