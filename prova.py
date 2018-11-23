edf10c6222e282f811a91957de714f0861e46b03
Domenico Caruso <domenico.caruso@de.clara.net>
2018-11-23 08:37:22 +0100

    agiornamento

--- prova.py
+++ prova.py
@@ -1,7 +1,8 @@
 import json
+from dateutil.parser import parse
 
 categorie = {
-  "appartamento" : ["energieversorgung","evo","thomas reinecke","energievers","XENOS","friedrich sommer"],
+  "appartamento" : ["energieversorgung","evo","thomas reinecke","energievers","XENOS","friedrich sommer","tilgung","zinsen"],
   "internet" : ["unitymedia"],
   "spesa" : ["rewe", "dm","tegut","scheck in","rossmann","meta","giovo","penny"],
   "acasa" : ["segmueller","galeria","bauhaus","toom","hornbach","fliesen","kibek","ikea","xxxl","obi","karstadt","staples"],
@@ -12,7 +13,9 @@ categorie = {
   "vacanze" : ["hotel"],
   "farmaci" : ["apotheke"],
   "trasporto" : ["vgf","verkehrs","db vertrieb"],
-  "uscite" : ["HESSEWIRTSCHAFT","LEONHARDS","GASTRONOMIE","TIGER","restaurant","pizzeria","jamys","WILLYS BAR","vapiano","biomarkt","coffee"],
+  "uscite" : ["HESSEWIRTSCHAFT","LEONHARDS","GASTRONOMIE","TIGER","restaurant","pizzeria","jamys","WILLYS BAR","vapiano","biomarkt","coffee","azurro","belge"],
+  "uscite_istruttive" : ["opel zoo","senckenberg"],
+  "prelievo" : ["ING-DiBa-AG"],
   "bici" : ["b.o.c"],
   "varie" : [],
   "nocat" : [],
@@ -27,41 +30,73 @@ def assign_to_cat (richiedente):
         return cat
   return "nocat"
 
+def clean_up (line):
+ try:
+   parse(line.split(";")[0])
+ except Exception:
+   print ("data sbagliata, riga ignorata")
+   print line
+   return False
+   
+
+ return True
+
+def check_richiedente(line,richiedente):
+  non_buono = ["Lastschrift aus","Caruso"]
+  causali_note = ["tilgung","zinsen"]
+  for each in non_buono:
+    if each.upper() in richiedente:
+      causale = line.split(";")[4]
+      for one in causali_note:
+        if one in causale:
+          return one.upper()
+      return causale.split("//")[0].upper()
+  return richiedente
+
 dizionario = {}
 
-spese = 0
 with open("estrattoConto.csv") as f: 
   for line in f.readlines():
-    data1 = line.split(";")[0]
-    try:
+    is_clean = clean_up(line)
+    if is_clean:
+      data1 = line.split(";")[0]
       mese = data1.split(".")[1]
       anno = data1.split(".")[2]
       mesanno = anno+"-"+mese
-    except Exception as e:
-      print ("warning", e)
-      continue
-    if mesanno not in dizionario.keys():
-      dizionario[mesanno] = {}
-      for cat in categorie.keys():
-        dizionario[mesanno][cat] = {}
+      if mesanno not in dizionario.keys():
+        dizionario[mesanno] = {}
+        for cat in categorie.keys():
+          dizionario[mesanno][cat] = {}
+
+      data2 = line.split(";")[1]
 
 
-    data2 = line.split(";")[1]
-    # TODO: chechk if richiedente is undefined
-    richiedente = line.split(";")[2].upper()
-    categoria = assign_to_cat(richiedente)
-    testo = line.split(";")[3]
-    causale = line.split(";")[4]
-    valuta = line.split(";")[6]
-    try:
-      importo = line.split(";")[5]
-      importo = float(importo.replace(",","."))
-    except:
-      continue
-    if not richiedente in dizionario[mesanno][categoria]:
-      dizionario[mesanno][categoria][richiedente] = importo
+      # TODO: chechk if richiedente is undefined
+      richiedente = line.split(";")[2].upper()
+      richiedente = check_richiedente(line,richiedente)
+      categoria = assign_to_cat(richiedente)
+      modalita = line.split(";")[3]
+      try:
+        causale = line.split(";")[4]
+      except:
+        print line
+      valuta = line.split(";")[6]
+      try:
+        importo = line.split(";")[5]
+        importo = importo.replace(".","")
+        importo = float(importo.replace(",","."))
+      except Exception as e:
+        print line
+        print e
+        continue
+      if "ING" in richiedente:
+        print categoria,importo
+      if not richiedente in dizionario[mesanno][categoria]:
+        dizionario[mesanno][categoria][richiedente] = importo
+      else:
+        dizionario[mesanno][categoria][richiedente] += importo
     else:
-      dizionario[mesanno][categoria][richiedente] += importo
+      continue
 
 print json.dumps(dizionario,sort_keys=True, indent=4)
 
