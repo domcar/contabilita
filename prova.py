a02eb3f74962a267b898352dc0084fc9fefaa428
Domenico Caruso <domenico.caruso@de.clara.net>
2018-11-25 14:39:53 +0100

    scrivi csv

--- prova.py
+++ prova.py
@@ -2,31 +2,40 @@ import json
 from dateutil.parser import parse
 
 categorie = {
-  "appartamento" : ["energieversorgung","evo","thomas reinecke","energievers","XENOS","friedrich sommer","tilgung","zinsen"],
-  "internet" : ["unitymedia"],
-  "spesa" : ["rewe", "dm","tegut","scheck in","rossmann","meta","giovo","penny"],
-  "acasa" : ["segmueller","galeria","bauhaus","toom","hornbach","fliesen","kibek","ikea","xxxl","obi","karstadt","staples"],
+  "abbigliamento" : ["h+m","woolworth","crocs","geox","baby walz","c+a","Ernsting","stoffe","schuhcenter"],
+  "acasa"         : ["segmueller","galeria","bauhaus","toom","hornbach",
+                     "fliesen","kibek","ikea","xxxl","obi","karstadt","staples",
+                     "weku","bm offenbach","tende","gries","idee.creativ","moemax"],
+  "appartamento"  : ["energieversorgung","evo","thomas reinecke","energievers","XENOS",
+                     "friedrich sommer","tilgung","zinsen","miete","taeglich",
+                     "giornaliero","baufinanzierung","weg","lanio","fbw"],
   "assicurazioni" : ["arag","huk","axa"],
-  "macchina" : ["driver center","akf bank","esso","aral","park","parking","calpam","shell","fraport"],
-  "tasse" : ["hcc"],
-  "nina" : ["tagtraume","familien kasse","toys"],
-  "vacanze" : ["hotel"],
-  "farmaci" : ["apotheke"],
-  "trasporto" : ["vgf","verkehrs","db vertrieb"],
-  "uscite" : ["HESSEWIRTSCHAFT","LEONHARDS","GASTRONOMIE","TIGER","restaurant","pizzeria","jamys","WILLYS BAR","vapiano","biomarkt","coffee","azurro","belge"],
-  "uscite_istruttive" : ["opel zoo","senckenberg"],
-  "prelievo" : ["ING-DiBa-AG"],
-  "bici" : ["b.o.c"],
-  "varie" : [],
-  "nocat" : [],
-  "abbigliamento" : ["h+m","woolworth","crocs","geox","baby walz","c+a"]
+  "bici"          : ["b.o.c","montimare","fahrrad"],
+  "entrate"       : ["entrate"],
+  "farmaci"       : ["apotheke","farmacia"],
+  "internet"      : ["unitymedia"],
+  "macchina"      : ["driver center","akf bank","esso","aral","park","parking",
+                     "calpam","shell","fraport"],
+  "nina"          : ["tagtraume","familien kasse","toys","nanu","primigi","piccoli"],
+  "nocat"         : [],
+  "prelievo"      : ["ING-DiBa-AG"],
+  "spesa"         : ["rewe", "dm","tegut","scheck in","rossmann","meta","giovo",
+                     "penny","supermercato","kafein","basic","europa commerciale","hema"],
+  "tasse"         : ["hcc","vanessa","gerichtskasse"],
+  "trasporto"     : ["vgf","verkehrs","db vertrieb"],
+  "uscite"        : ["HESSEWIRTSCHAFT","LEONHARDS","GASTRONOMIE","TIGER","restaurant",
+                     "pizzeria","jamys","WILLYS BAR","vapiano","biomarkt","coffee",
+                     "azurro","belge","dolce vita","speckwirt","aplofoods","TURM-BRAEU"],
+  "fuori-casa"    : ["opel zoo","senckenberg"],
+  "vacanze"       : ["hotel"],
+  "varie"         : ["ofc fanshop","gulliver","relay"]
 }
 
 def assign_to_cat (richiedente):
 
   for cat in categorie.keys():
     for subcat in categorie[cat]:
-      if subcat.upper() in richiedente:
+      if subcat.upper() in richiedente.upper():
         return cat
   return "nocat"
 
@@ -41,15 +50,18 @@ def clean_up (line):
 
  return True
 
-def check_richiedente(line,richiedente):
-  non_buono = ["Lastschrift aus","Caruso"]
-  causali_note = ["tilgung","zinsen"]
+def check_richiedente(line,richiedente,importo):
+  if importo > 0:
+    return "entrate"
+
+  non_buono = ["Lastschrift aus","Caruso","Belyaeva","First Data"]
+  causali_note = ["tilgung","zinsen","miete","taeglich","giornaliero","baufinanzierung"]
   for each in non_buono:
-    if each.upper() in richiedente:
+    if each.upper() in richiedente.upper():
       causale = line.split(";")[4]
-      for one in causali_note:
-        if one in causale:
-          return one.upper()
+      for noto in causali_note:
+        if noto.upper() in causale.upper():
+          return noto.upper()
       return causale.split("//")[0].upper()
   return richiedente
 
@@ -69,28 +81,28 @@ with open("estrattoConto.csv") as f:
           dizionario[mesanno][cat] = {}
 
       data2 = line.split(";")[1]
-
-
-      # TODO: chechk if richiedente is undefined
-      richiedente = line.split(";")[2].upper()
-      richiedente = check_richiedente(line,richiedente)
-      categoria = assign_to_cat(richiedente)
-      modalita = line.split(";")[3]
-      try:
-        causale = line.split(";")[4]
-      except:
-        print line
-      valuta = line.split(";")[6]
       try:
         importo = line.split(";")[5]
         importo = importo.replace(".","")
         importo = float(importo.replace(",","."))
       except Exception as e:
-        print line
+        print "importo errato, ignorato"
         print e
         continue
-      if "ING" in richiedente:
-        print categoria,importo
+
+
+      richiedente = line.split(";")[2].upper()
+      richiedente = check_richiedente(line,richiedente,importo)
+      categoria = assign_to_cat(richiedente)
+      #if categoria == "nocat":
+      #  print categoria, line
+      modalita = line.split(";")[3]
+      try:
+        causale = line.split(";")[4]
+      except:
+        print "causale sbagliata"
+        print line
+      valuta = line.split(";")[6]
       if not richiedente in dizionario[mesanno][categoria]:
         dizionario[mesanno][categoria][richiedente] = importo
       else:
@@ -98,5 +110,9 @@ with open("estrattoConto.csv") as f:
     else:
       continue
 
-print json.dumps(dizionario,sort_keys=True, indent=4)
+#print json.dumps(dizionario,sort_keys=True, indent=4)
 
+for data in dizionario.keys():
+  for cat in dizionario[data].keys():
+    for subcat in dizionario[data][cat].keys():
+      print (data+";"+cat+";"+subcat+";"+str(dizionario[data][cat][subcat]))
