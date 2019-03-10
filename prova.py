670ea3198bb9cc3c4178b5f2ad33c23e2834fc62
domenico-casa <d-caruso@hotmail.it>
2019-03-10 11:19:47 +0100

    buh

--- prova.py
+++ prova.py
@@ -4,10 +4,10 @@ from dateutil.parser import parse
 # categorie e sottocategorie
 categorie = {
   "abbigliamento" : ["h+m","woolworth","crocs","geox","baby walz","c+a","Ernsting","stoffe","schuhcenter","totale"],
-  "acasa"         : ["segmueller","galeria","bauhaus","toom","hornbach",
+  "acasa"         : ["segmueller","galeria","bauhaus","toom","hornbach", "saturn",
                      "fliesen","kibek","ikea","xxxl","obi","karstadt","staples",
-                     "weku","bm offenbach","tende","gries","idee.creativ","moemax","totale","BDSK HANDELS"],
-  "appartamento"  : ["energieversorgung","evo","thomas reinecke","energievers","XENOS",
+                     "weku","bm offenbach","tende","gries","idee.creativ","moemax","totale","bdsk handels"],
+  "appartamento"  : ["energieversorgung","evo","thomas reinecke","energievers","xenos",
                      "friedrich sommer","tilgung","zinsen","miete","taeglich",
                      "giornaliero","baufinanzierung","weg","lanio","fbw","totale"],
   "assicurazioni" : ["arag","huk","axa","totale","kfz versicherung","techniker krank"],
@@ -17,19 +17,19 @@ categorie = {
   "internet"      : ["unitymedia","totale"],
   "macchina"      : ["driver center","akf bank","esso","aral","park","parking",
                      "calpam","shell","fraport","totale"],
-  "nina"          : ["tagtraume","TAGTRAEUME","familien kasse","toys","nanu","primigi","piccoli","totale"],
+  "nina"          : ["tagtraume","tagtraeume","familien kasse","toys","nanu","primigi","piccoli","totale"],
   "nocat"         : ["totale"],
   "prelievo"      : ["ING-DiBa-AG","totale"],
-  "spesa"         : ["rewe", "dm","tegut","scheck in","rossmann","meta","giovo",
-                     "penny","supermercato","kafein","basic","europa commerciale","hema","totale"],
-  "tasse"         : ["hcc","vanessa","gerichtskasse","totale"],
+  "spesa"         : ["rewe", "dm","tegut","scheck in","rossmann","meta","giovo","buhara","mix markt",
+                     "penny","supermercato","kafein","basic","europa commerciale","hema","totale",
+                     "DIETZENBACHER MARKT"],
+  "tasse"         : ["hcc","vanessa","gerichtskasse","totale","stadt offenbach"],
   "trasporto"     : ["vgf","verkehrs","db vertrieb","totale"],
-  "uscite"        : ["HESSEWIRTSCHAFT","LEONHARDS","GASTRONOMIE","TIGER","restaurant",
-                     "pizzeria","jamys","WILLYS BAR","vapiano","biomarkt","coffee",
+  "uscite"        : ["hessewirtschaft","leonhards","gastronomie","tiger","restaurant", "pistazie",
+                     "pizzeria","jamys","willys bar","vapiano","biomarkt","coffee",
                      "azurro","belge","dolce vita","speckwirt","aplofoods","TURM-BRAEU","totale","gastronom"],
-  "fuori-casa"    : ["opel zoo","senckenberg","totale"],
   "vacanze"       : ["hotel","totale"],
-  "varie"         : ["ofc fanshop","gulliver","relay","totale"]
+  "varie"         : ["ofc fanshop","gulliver","relay","totale","opel zoo","senckenberg"]
 }
 
 def assign_to_cat (richiedente):
@@ -55,7 +55,7 @@ def check_richiedente(line,richiedente,importo):
   if importo > 0:
     return "entrate"
 
-  non_buono = ["Lastschrift aus","Caruso","Belyaeva","First Data"]
+  non_buono = ["Lastschrift aus","Caruso","Belyaeva","First Data","BUS ZWISCHENKONTO"]
   causali_note = ["tilgung","zinsen","miete","taeglich","giornaliero","baufinanzierung"]
   for each in non_buono:
     if each.upper() in richiedente.upper():
@@ -66,9 +66,31 @@ def check_richiedente(line,richiedente,importo):
       return causale.split("//")[0].upper()
   return richiedente
 
+#data   ;abbigliamento;acasa;entrate;farmaci;macchina;nina;nocat;spesa;trasporto;
+#2017-12;57.5         ;52   ;1979   ;7;59   ;591     ;0   ;451        ;95
+
+def print_to_file():
+  ris_tot = open("risultato_totale.csv","w")
+  ris_tot.write("data"+";")
+  for cat in sorted(categorie.keys()):
+    ris_tot.write(cat+";")
+  ris_tot.write("\n")
+
+  for data in sorted(dizionario.keys()):
+    ris_tot.write(data+";")
+    for cat in sorted(categorie.keys()):
+      if dizionario[data][cat]:
+        for subcat in sorted(dizionario[data][cat].keys()):
+          if subcat == "totale": 
+            ris_tot.write(str(abs(dizionario[data][cat][subcat])).replace(".",",")+";")
+      else:
+        ris_tot.write("0;")
+    ris_tot.write("\n")
+    
+
 dizionario = {}
 
-with open("estrattoConto.csv") as f: 
+with open("estrattoConto_1-dic-2017_28-feb-2019.csv") as f: 
   for line in f.readlines():
     is_clean = clean_up(line)
     if is_clean:
@@ -111,15 +133,6 @@ with open("estrattoConto.csv") as f:
     else:
       continue
 
-#print json.dumps(dizionario,sort_keys=True, indent=4)
+print json.dumps(dizionario,sort_keys=True, indent=4)
+print_to_file()
 
-ris_tot = open("risultato_totale.csv","w")
-ris_sot = open("risultato_sottocategoria.csv","w")
-for data in sorted(dizionario.keys()):
-  for cat in dizionario[data].keys():
-    for subcat in dizionario[data][cat].keys():
-      if subcat == "totale": 
-        if not dizionario[data][cat][subcat] == 0:
-          ris_tot.write(data+";"+cat+";"+str(abs(dizionario[data][cat][subcat]))+"\n")
-      else:
-        ris_sot.write(data+";"+cat+";"+subcat+";"+str(dizionario[data][cat][subcat])+"\n")
